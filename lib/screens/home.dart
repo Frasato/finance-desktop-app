770e05c85cc356d8011897bfd53f3f95b3aa5420
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-04 15:57:39 -0300

    Add Side Menu component on home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
+import 'package:desktop_app/components/side_menu.dart';
 import 'package:desktop_app/constants/colors.dart';
-import 'package:desktop_app/widgets/side_button.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -12,30 +12,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: backgroundColor,
       body: Row(
         children: [
+          SideMenu(),
           Expanded(
-            flex: 1,
-            child: Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  right: BorderSide(color: lightGrey, width: 1)
-                )
-              ),
-              child: Column(
-                children: [
-                  const Text('Hello'),
-                  const Text('Frasato'),
-                  SideButton(onPressed: (){}, buttonText: 'Home', icon: Icons.home,),
-                  SideButton(onPressed: (){}, buttonText: 'Cash in', icon: Icons.keyboard_double_arrow_right_outlined,),
-                  SideButton(onPressed: (){}, buttonText: 'Cash out', icon: Icons.keyboard_double_arrow_left_outlined,),
-                ],
-              ),
-            ),
-          ),
-          const Expanded(
             flex: 3,
             child: Column(
               children: [],
