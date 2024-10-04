49625dc2390d443a4029a8fadecd06fc310b6cd5
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-04 15:57:02 -0300

    Create Side Menu component

---
+++ lib/components/side_menu.dart
@@ -0,0 +1,44 @@
+import 'package:desktop_app/constants/colors.dart';
+import 'package:desktop_app/widgets/side_button.dart';
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            right: BorderSide(color: lightGrey, width: 1)
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 25, bottom: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const Text('Hello'),
+                  const Text('Frasato'),
+                  SideButton(onPressed: (){}, buttonText: 'Home', icon: Icons.home,),
+                  SideButton(onPressed: (){}, buttonText: 'Cash in', icon: Icons.keyboard_double_arrow_right_outlined,),
+                  SideButton(onPressed: (){}, buttonText: 'Cash out', icon: Icons.keyboard_double_arrow_left_outlined,),
+                ],
+              ),
+              SideButton(onPressed: (){}, buttonText: 'Logout', icon: Icons.logout,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
