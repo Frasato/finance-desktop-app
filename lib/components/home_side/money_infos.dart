d8e5be2a20ef277bf54b5f346b54194cdbc8a22b
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-05 11:33:10 -0300

    Create money infos component

---
+++ lib/components/home_side/money_infos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoneyInfos extends StatefulWidget {
+  const MoneyInfos({super.key});
+
+  @override
+  State<MoneyInfos> createState() => _MoneyInfosState();
+}
+
+class _MoneyInfosState extends State<MoneyInfos> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
