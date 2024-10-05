9362a0301bbddb64caf627eb4c1a774877b30ab6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-05 11:31:15 -0300

    Create Last Operation component

---
+++ lib/components/home_side/last_operation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LastOperation extends StatefulWidget {
+  const LastOperation({super.key});
+
+  @override
+  State<LastOperation> createState() => _LastOperationState();
+}
+
+class _LastOperationState extends State<LastOperation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
