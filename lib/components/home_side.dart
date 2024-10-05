1188ed1dc7eebdf1a3e8b0e40c8db7558c59fddf
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-05 11:24:49 -0300

    Create Home Side component

---
+++ lib/components/home_side.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeSide extends StatefulWidget {
+  const HomeSide({super.key});
+
+  @override
+  State<HomeSide> createState() => _HomeSideState();
+}
+
+class _HomeSideState extends State<HomeSide> {
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      flex: 4,
+      child: Column(
+        children: [],
+      )
+    );
+  }
+}
