46e74996b5e98b5ea2a158514689f02695be67d6
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-07 10:55:20 -0300

    Create money infos widget

---
+++ lib/widgets/money_infos_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MoneyInfosItem extends StatefulWidget {
+  const MoneyInfosItem({super.key});
+
+  @override
+  State<MoneyInfosItem> createState() => _MoneyInfosItemState();
+}
+
+class _MoneyInfosItemState extends State<MoneyInfosItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [],
+    );
+  }
+}
