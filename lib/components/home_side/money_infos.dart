b5f1e52fd7c28061a1d70fed64784ef07b47ac96
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-10-07 11:56:04 -0300

    Create first column

--- lib/components/home_side/money_infos.dart
+++ lib/components/home_side/money_infos.dart
@@ -1,3 +1,4 @@
+import 'package:desktop_app/widgets/money_infos_item.dart';
 import 'package:flutter/material.dart';
 
 class MoneyInfos extends StatefulWidget {
@@ -13,8 +14,13 @@ class _MoneyInfosState extends State<MoneyInfos> {
     return const Row(
       children: [
         Column(
-          children: [],
-        )
+          children: [
+            MoneyInfosItem(cost: '\$300,00', icon: Icons.lunch_dining, title: 'Food',),
+            MoneyInfosItem(cost: '\$1200,00', icon: Icons.house, title: 'Home Bill',),
+            MoneyInfosItem(cost: '\$600,00', icon: Icons.pedal_bike_rounded, title: 'Travels',),
+            MoneyInfosItem(cost: '\$125,00', icon: Icons.school, title: 'Learning',),
+          ],
+        ),
       ],
     );
   }
